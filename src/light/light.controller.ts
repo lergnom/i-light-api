49691c72671439f38705af1e36c245920e023a0e
lergnom <lergnom@mail.ru>
2022-02-07 15:10:53 +0300

    add net and PromiseSocket

--- src/light/light.controller.ts
+++ src/light/light.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, Post } from '@nestjs/common';
+import { LightService } from './light.service';
 
 @Controller('light')
-export class LightController {}
+export class LightController {
+  constructor(private readonly lightService: LightService) {
+  }
+  @Post('light')
+  @HttpCode(200)
+  async lightOn(){
+	return null;
+  }
+}
--- src/light/light.service.ts
+++ src/light/light.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import net from 'net';
+import {PromiseSocket} from 'promise-socket';
+import { IPConfig, Light } from './light.constants';
+const socket = new net.Socket();
+const client = new PromiseSocket(socket);
 
 @Injectable()
-export class LightService {}
+export class LightService {
+
+  async status(){
+	client.connect(IPConfig.port, IPConfig.host).then(res => {
+		socket.write(Light.status , function () {Logger.log('Send command status');
+		});
+		socket.on('data', function (data) {
+		Logger.log('response:' + data.toString());
+		if (data.toString() === '00000000') {
+		Logger.log('status 0ff');
+			socket.end();
+		}
+		});
+	});
+  }
+  async lightOn(){
+  return 'on';
+  }
+}
