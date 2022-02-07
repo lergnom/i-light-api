fc766e03a72143575aca410eed0c0812c8af042a
lergnom <lergnom@mail.ru>
2022-02-07 14:23:46 +0300

    added files for light

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { LightController } from './light/light.controller';
+import { LightService } from './light/light.service';
+import { LightModule } from './light/light.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, LightModule],
+  controllers: [AppController, LightController],
+  providers: [AppService, LightService],
 })
 export class AppModule {}
---
+++ src/light/light.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('light')
+export class LightController {}
