9d9326ebd14a7da600501e7f5ec4f21f9941f8ac
lergnom <lergnom@mail.ru>
2022-02-08 09:31:29 +0300

    added microservice

--- src/light/light.service.ts
+++ src/light/light.service.ts
@@ -1,27 +1,35 @@
 import { Injectable, Logger } from '@nestjs/common';
-import net from 'net';
-import {PromiseSocket} from 'promise-socket';
 import { IPConfig, Light } from './light.constants';
-const socket = new net.Socket();
-const client = new PromiseSocket(socket);
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+
 @Injectable()
 export class LightService {
+  private client: ClientProxy;
 
-
-  async getStatus(){
-	client.connect(IPConfig.port, IPConfig.host).then(res => {
-		socket.write(Light.status , function () {Logger.log('Send command status');
-		});
-		socket.on('data', function (data) {
-		Logger.log('response:' + data.toString());
-		if (data.toString() === '00000000') {
-		Logger.log('status 0ff');
-			socket.end();
-		}
-		});
+  constructor() {
+	this.client = ClientProxyFactory.create({
+		transport: Transport.TCP,
+		options: { host: IPConfig.host, port: IPConfig.port }
 	});
   }
-  async lightOn(){
-  return 'on';
+
+  async getStatus() {
+	Logger.log('check getStatus');
+	return this.client.send('status', Light.status);
+	// client.connect(IPConfig.port, IPConfig.host).then(res => {
+	// 	socket.write(Light.status , function () {Logger.log('Send command status');
+	// 	});
+	// 	socket.on('data', function (data) {
+	// 	Logger.log('response:' + data.toString());
+	// 	if (data.toString() === '00000000') {
+	// 	Logger.log('status 0ff');
+	// 		socket.end();
+	// 	}
+	// });
+	// });
+  }
+
+  async lightOn() {
+	return 'on';
   }
 }
