dd41e86069915117bbe070694b9e2caee1a08a0a
lergnom <lergnom@mail.ru>
2022-02-09 09:08:29 +0300

    added catch in promise

--- src/light/light.service.ts
+++ src/light/light.service.ts
@@ -5,9 +5,7 @@ import PromiseSocket from 'promise-socket';
 @Injectable()
 export class LightService {
   private client = new PromiseSocket();
-	public status: string;
-
-
+	private status: string;
 
   // async getStatus() {
 	// 	const st = {
@@ -32,39 +30,50 @@ export class LightService {
 	 return this.status;
  }
  getStatusNow(){
+	 Logger.log('check status');
 		return this.status;
+
  }
 
 	async getStatus() {
+		Logger.log('send command status');
 		this.client.connect(IPConfig.port, IPConfig.host).then(() => {
 		this.client.socket.write(Light.status, () => {
 		});
 	this.client.socket.on('data', (data) => {
 		this.getState(data.toString());
 		});
-	});
+	}).catch(reason => {
+		Logger.log('reason ' + reason);
+		});
 	}
 	
 
   async lightOn() {
 		Logger.log('send command on');
-		await this.client.connect(IPConfig.port, IPConfig.host).then(res => {
+		 this.client.connect(IPConfig.port, IPConfig.host).then(() => {
 			this.client.socket.write(Light.on, () => {
 			});
-			this.client.socket.on('data', function(data) {
+			this.client.socket.on('data', (data) => {
+				this.getState(data.toString());
 			});
 			this.client.socket.end();
-		});
+		}).catch(reason => {
+			 Logger.log('reason ' + reason);
+		 });
 	}
 
 	async lightOff() {
 		Logger.log('send command off');
-		await this.client.connect(IPConfig.port, IPConfig.host).then(res => {
+		this.client.connect(IPConfig.port, IPConfig.host).then(res => {
 			this.client.socket.write(Light.off, () => {
 			});
-			this.client.socket.on('data', function(data) {
+			this.client.socket.on('data', (data) => {
+				this.getState(data.toString());
 			});
 			this.client.socket.end();
+		}).catch(reason => {
+			Logger.log('reason ' + reason);
 		});
 	}
 }
