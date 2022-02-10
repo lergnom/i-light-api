c9f7b22ae5af5df311037008b5294eec190c92a3
lergnom <lergnom@mail.ru>
2022-02-10 09:54:20 +0300

    refactor getconnect function

--- src/light/light.service.ts
+++ src/light/light.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { IPConfig, Light } from './light.constants';
+import { IPConfig, Light, LightType } from "./light.constants";
 import PromiseSocket from 'promise-socket';
 
 @Injectable()
@@ -16,44 +16,60 @@ export class LightService {
 		return this.status;
  }
 
+ async getConnect(cmd: string ){
+	 Logger.log(`send command   ${cmd}`);
+	 this.client.connect(IPConfig.port, IPConfig.host).then(() => {
+		 this.client.socket.write(cmd, () => {
+		 });
+		 this.client.socket.on('data', (data) => {
+			 this.getState(data.toString());
+		 });
+	 }).catch(reason => {
+		 Logger.log('reason ' + reason);
+	 });
+ }
+
 	async getStatus() {
-		Logger.log('send command status');
-		this.client.connect(IPConfig.port, IPConfig.host).then(() => {
-		this.client.socket.write(Light.status, () => {
-		});
-	this.client.socket.on('data', (data) => {
-		this.getState(data.toString());
-		});
-	}).catch(reason => {
-		Logger.log('reason ' + reason);
-		});
+		await this.getConnect(Light.status);
+	// 	Logger.log('send command status');
+	// 	this.client.connect(IPConfig.port, IPConfig.host).then(() => {
+	// 	this.client.socket.write(Light.status, () => {
+	// 	});
+	// this.client.socket.on('data', (data) => {
+	// 	this.getState(data.toString());
+	// 	});
+	// }).catch(reason => {
+	// 	Logger.log('reason ' + reason);
+	// 	});
 	}
 
   async lightOn() {
-		Logger.log('send command on');
-		 this.client.connect(IPConfig.port, IPConfig.host).then(() => {
-			this.client.socket.write(Light.on, () => {
-			});
-			this.client.socket.on('data', (data) => {
-				this.getState(data.toString());
-			});
-			this.client.socket.end();
-		}).catch(reason => {
-			 Logger.log('reason ' + reason);
-		 });
+		await this.getConnect(Light.on);
+		// Logger.log('send command on');
+		//  this.client.connect(IPConfig.port, IPConfig.host).then(() => {
+		// 	this.client.socket.write(Light.on, () => {
+		// 	});
+		// 	this.client.socket.on('data', (data) => {
+		// 		this.getState(data.toString());
+		// 	});
+		// 	this.client.socket.end();
+		// }).catch(reason => {
+		// 	 Logger.log('reason ' + reason);
+		//  });
 	}
 
 	async lightOff() {
-		Logger.log('send command off');
-		this.client.connect(IPConfig.port, IPConfig.host).then(res => {
-			this.client.socket.write(Light.off, () => {
-			});
-			this.client.socket.on('data', (data) => {
-				this.getState(data.toString());
-			});
-			this.client.socket.end();
-		}).catch(reason => {
-			Logger.log('reason ' + reason);
-		});
+		await this.getConnect(Light.off);
+	// 	Logger.log('send command off');
+	// 	this.client.connect(IPConfig.port, IPConfig.host).then(res => {
+	// 		this.client.socket.write(Light.off, () => {
+	// 		});
+	// 		this.client.socket.on('data', (data) => {
+	// 			this.getState(data.toString());
+	// 		});
+	// 		this.client.socket.end();
+	// 	}).catch(reason => {
+	// 		Logger.log('reason ' + reason);
+	// 	});
 	}
 }
