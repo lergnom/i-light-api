0539cb5d370d5813c8813089432b7ca3f703cf2a
lergnom <lergnom@mail.ru>
2022-02-11 08:59:03 +0300

    refactoring

--- src/light/light.service.ts
+++ src/light/light.service.ts
@@ -31,45 +31,13 @@ export class LightService {
 
 	async getStatus() {
 		await this.getConnect(Light.status);
-	// 	Logger.log('send command status');
-	// 	this.client.connect(IPConfig.port, IPConfig.host).then(() => {
-	// 	this.client.socket.write(Light.status, () => {
-	// 	});
-	// this.client.socket.on('data', (data) => {
-	// 	this.getState(data.toString());
-	// 	});
-	// }).catch(reason => {
-	// 	Logger.log('reason ' + reason);
-	// 	});
 	}
 
   async lightOn() {
 		await this.getConnect(Light.on);
-		// Logger.log('send command on');
-		//  this.client.connect(IPConfig.port, IPConfig.host).then(() => {
-		// 	this.client.socket.write(Light.on, () => {
-		// 	});
-		// 	this.client.socket.on('data', (data) => {
-		// 		this.getState(data.toString());
-		// 	});
-		// 	this.client.socket.end();
-		// }).catch(reason => {
-		// 	 Logger.log('reason ' + reason);
-		//  });
 	}
 
 	async lightOff() {
 		await this.getConnect(Light.off);
-	// 	Logger.log('send command off');
-	// 	this.client.connect(IPConfig.port, IPConfig.host).then(res => {
-	// 		this.client.socket.write(Light.off, () => {
-	// 		});
-	// 		this.client.socket.on('data', (data) => {
-	// 			this.getState(data.toString());
-	// 		});
-	// 		this.client.socket.end();
-	// 	}).catch(reason => {
-	// 		Logger.log('reason ' + reason);
-	// 	});
 	}
 }
